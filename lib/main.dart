fc110a9eb388bd45c927d3ce76f7977e542b454c
Kacper <wysockikacper98@gmail.com>
2021-05-07 19:07:39 +0200

    Develop Init
    Took 54 seconds

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("build -> main.dart");
-    // Custom Colors
-    // const MaterialColor primaryColor = const MaterialColor(
-    //   0xFFFC9684,
-    //   const <int, Color>{
-    //     50: const Color(0xFFFC9684),
-    //     100: const Color(0xFFFC9684),
-    //     200: const Color(0xFFFC9684),
-    //     300: const Color(0xFFFC9684),
-    //     400: const Color(0xFFFC9684),
-    //     500: const Color(0xFFFC9684),
-    //     600: const Color(0xFFFC9684),
-    //     700: const Color(0xFFFC9684),
-    //     800: const Color(0xFFFC9684),
-    //     900: const Color(0xFFFC9684),
-    //   },
-    // );
 
     return MaterialApp(
       title: 'FixIT!',
