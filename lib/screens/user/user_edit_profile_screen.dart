7fab4c964dbe757945a95b80226d418339eca5d0
Kacper <wysockikacper98@gmail.com>
2021-06-25 16:56:37 +0200

    Widok avatara i imienia + nazwiska (tylko wersja wizualna)
    Took 42 minutes

--- lib/screens/user/user_edit_profile_screen.dart
+++ lib/screens/user/user_edit_profile_screen.dart
@@ -14,16 +14,70 @@ class _UserEditProfileScreenState extends State<UserEditProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final sizeMediaQuery = MediaQuery.of(context).size;
+    final width = sizeMediaQuery.width;
+    // final height = sizeMediaQuery.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Profil Użytkownika"),
       ),
       body: FutureBuilder(
-        future: FirebaseFirestore.instance.collection('users').doc(userID).get(),
+        future:
+            FirebaseFirestore.instance.collection('users').doc(userID).get(),
         builder: (_, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             //TODO: Widok do edycji użytkownika
-            return Center(child: Text(snapshot.data.data().toString()));
+            // return Center(child: Text(snapshot.data.data().toString()));
+            final user = snapshot.data.data();
+            return Container(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.orangeAccent.shade100,
+                        radius: width * 0.15,
+                      ),
+                      Positioned(
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.insert_photo,
+                            size: width * 0.08,
+                          ),
+                          onPressed: () {
+                            print('Pick new Photo');
+                          },
+                        ),
+                        bottom: 0,
+                        right: 0,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  Stack(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.center,
+                    children: [
+                      Text(
+                        user['firstName'] + ' ' + user['lastName'],
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Positioned(
+                        child: IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () {},
+                        ),
+                        right: -40,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
           } else if (snapshot.data == null) {
             return Center(child: Text("User not found"));
           } else {
